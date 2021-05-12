6b15fe115aa155db3c81a80b2f6343c329833b4d
Eliecer <eliecer.aguilar1@utp.ac.pa>
2021-05-12 12:51:54 -0500

    actualizacion y mejoras

--- menu.sh
+++ menu.sh
@@ -1,54 +1,104 @@
 #!/bin/bash
 
 #menu
-opciones="Verificar Ensamblar Crear_GIF Salir"
+
+opciones="Verificar Ensamblar CrearGIF Salir"
+
 function verificar {
-	echo "Ingrese ruta de acceso a las imágenes"
-		read directorio
-		#directorio="/home/erod10/"
-		if [[ -d $directorio ]]; then
-			echo "La ruta existe y es: "$(pwd)
-			echo "Kbytes	Archivos.png"
-			f=$(ls -sh *.png)
-			for i in f;
-			do
-				l=$(($l+1))
-				echo "$f"
-			done
-		elif [[ -f $directorio ]]; then
-			echo "Es una ruta...pero de un archivo"
-		fi
+	echo "Ingrese ruta de acceso a las imagenes"
+	read directorio
+
+	if [[ -d $directorio]]; then
+		echo "La ruta existe y es: $directorio"
+		cd $directorio
+		echo "Kbytes  Archivos.png"
+		f=s(ls -sh *.png)
+		for i in f;
+		do
+			# l=$(($l+1))
+			echo $f
+		done
+	elif [[-f $directorio]];then
+		echo "Es una ruta... pero de un archivo"
+	fi
+
 }
 
-function ensamblar {
-	echo "Hola! Ahora trabajaremos desde APNG Assembler (apngasm)"
+function ensamblar{
+	echo "Hola! Ahora trabajaremos desde APNG Assembler	(apngasm)"
+
+	if [[-d $directorio]]; then
+		echo "La ruta es: $directorio"
+		cd $directorio
+		echo "Archivos.png"
+		f=$(ls *.png)
+		for ((i=0; i<7; i++));
+		do
+			#l = $(($l+1))
+			echo $f
+		done
+		echo "Ingrese el nombre del archivo de salida"
+		read file_name
+		echo "Ingrese el retraso o delay"
+		read file_delay
+		echo "Ingrese la cantidad de ciclos a animar"
+		read file_frame
+
+		apngasm -o $file_name.png $f $file_delay -l$file_frame
+		a=$(pwd)
+		if [-d "$a/PNG2GIFT"]; then
+			echo "La carperta PNG2GIFT existe"
+		else
+			mkdir $a/PNG2GIFT
+			echo "Se ha creado la ruta $a/PNG2GIFT"
+		fi
+		mv $file_name.png $a/PNG2GIFT
+	elif [[ -f $directorio ]];then
+		echo "Es una ruta ... pero de un archivo"
+	fi
+
+}
+
+
+function crear_GIF{
+	echo "Hola! ahora crearemos un gif (apn2gif)"
+
 	if [[ -d $directorio ]]; then
-			echo "La ruta existe y es: "$(pwd)
-			echo "Kbytes	Archivos.png"
-			f=$(ls -sh *.png)
-			for i in f;
-			do
-				l=$(($l+1))
-				echo "$f"
-			done
-		elif [[ -f $directorio ]]; then
-			echo "Es una ruta...pero de un archivo"
+		cd directorio
+		b=$(pwd)
+		if [ -d $b/PNG2GIFT ];then
+			echo "La carperta PNG2GIFT existe"
+		else
+			mkdir $b/PNG2GIFT
+			echo "Se ha creado la ruta $b/PNG2GIFT"
 		fi
-	echo "ingrese el nombre del archivo de salida"
-	read file_name
-	echo "ingrese el retraso o delay"
-	read file_delay
-	echo "ingrese cantidad de ciclos a animar"
-	read file_frame
 
-	apngasm -o $file_name.png clock*.png $file_delay -l$file_frame
+		c="$b/PNG2GIFT"
+		echo "Ruta en la que estamos $c"
+		cd $c
+		echo "Archivos.png"
+		f=$(ls *.png)
+		
+		for ((i=0; i<1; i++));
+		do
+			echo "$f"
+		done
+		
+		echo "Seleccione de la lista el archivo a trabajar y escribalo en el siguiente campo"
+		echo "ingrese el nombre del archivo de entrada"
+		read file_in_name
+		echo "ingrese el nombre del archivo de salida"
+		read file_out_name
+
+		apng2gif $file_in_name.png $file_out_name.gif
+		mv $file_out_name.gif $c
+	elif [[ -f $directorio ]]; then
+		echo "Es una ruta...pero de un archivo"
+	fi
 }
 
-function crear_GIF {
-	echo "Hola desde la funcion!"
-}
-
-echo "Selecciona una opción de nuestro menu"
+echo "Bienvenido a Manejo de Archivos en BASH!"
+echo "Ahora, selecciona una opciรณn de nuestro menu"
 select opcion in $opciones;
 do
 	if [ $opcion = "Verificar" ]; then
@@ -61,6 +111,7 @@ do
 		echo "Bye!"
 		exit
 	else
-		echo "Opcion no válida!"
+		echo "Opcion no valida!"
 	fi
 done
+}
